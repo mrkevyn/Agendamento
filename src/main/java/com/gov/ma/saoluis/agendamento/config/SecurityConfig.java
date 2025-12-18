e96549444d13c65a0f6a0f7736ed68e0c446cd92
mrkevyn <kevyn.carlos@hotmail.com>
2025-12-18 17:58:24 -0300

    att

--- src/main/java/com/gov/ma/saoluis/agendamento/config/SecurityConfig.java
+++ src/main/java/com/gov/ma/saoluis/agendamento/config/SecurityConfig.java
@@ -21,8 +21,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http
-                .csrf(csrf -> csrf.disable())
+        http.csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
 

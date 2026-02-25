f67b7149256f2bc3eacc70fa0fe05f606b540e23
mrkevyn <kevyn.carlos@hotmail.com>
2026-02-25 11:54:33 -0300

    rota do telao aberta

--- src/main/java/com/gov/ma/saoluis/agendamento/config/SecurityConfig.java
+++ src/main/java/com/gov/ma/saoluis/agendamento/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                                 "/uploads/**",
                                 "/secretarias/**",
                                 "/setores/**",
+                                "/agendamentos/ultimas-chamadas/**",
 
                                 // 🔹 Swagger / OpenAPI
                                 "/v3/api-docs/**",

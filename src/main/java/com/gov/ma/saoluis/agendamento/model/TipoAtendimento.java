bc64879922171b24f9a31439ba17e92368b49e31
mrkevyn <kevyn.carlos@hotmail.com>
2026-04-05 12:47:31 -0300

    aumentando o length de nome do tipo_atendimento

--- src/main/java/com/gov/ma/saoluis/agendamento/model/TipoAtendimento.java
+++ src/main/java/com/gov/ma/saoluis/agendamento/model/TipoAtendimento.java
@@ -9,7 +9,7 @@ public class TipoAtendimento {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 500)
+    @Column(nullable = false, length = 100)
     private String nome;
 
     @Column(nullable = false, length = 1)
